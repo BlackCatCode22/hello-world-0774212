a334e04fdf0ccf6e195212f3acd34e54c1bc0a00
0774212 <lrd98@yahoo.com>
2024-08-13 11:44:30 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n\nHello World!");
+        System.out.println("\n\nHow are you gentlemen!!");
+        System.out.println("\n\nAll your base are belong to us.");
+    }
+}
